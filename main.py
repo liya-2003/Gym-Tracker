59cb8f4caae9b124eb0d63e931018782dac01edc
Tanjira Akther <tanjiraakther@Tanjiras-MacBook-Pro.local>
2026-07-08 20:59:23 +0100

    created main menu

---
+++ main.py
@@ -0,0 +1,14 @@
+print("=" * 30)
+print(" Welcome to Gym Tracker! ")
+print("=" * 30)
+
+print("\nMenu")
+print("1. Add Workout")
+print("2. View Workouts")
+print("3. Track Weight")
+print("4. View Progress")
+print("5. Exit")
+
+choice = input("\nChoose an option (1-5): ")
+
+print(f"\nYou selected option {choice}.")
